19659928d4b9c7863fe19f9df09f19a646d723e6
a.alghooneh@gmail.com <a.alghooneh@gmail.com>
2023-11-15 02:29:19 -0500

    interface added

--- particleFilter.py
+++ particleFilter.py
@@ -14,7 +14,7 @@ from utilities import *
 from rclpy.duration import Duration
 
 from visualization_msgs.msg import Marker, MarkerArray
-from geometry_msgs.msg import Point
+from geometry_msgs.msg import Point, PoseWithCovarianceStamped
 from std_msgs.msg import ColorRGBA
 from nav_msgs.msg import OccupancyGrid
 
@@ -23,7 +23,7 @@ from nav_msgs.msg import OccupancyGrid
 
 class particleFilter(Node):
 
-    def __init__(self, mapFilename="/home/dastan/final/maps/room.yaml", numParticles=1000):
+    def __init__(self, mapFilename="/home/dastan/final/maps/room.yaml", numParticles=500):
         
         super().__init__("particleFiltering")
 
@@ -38,34 +38,57 @@ class particleFilter(Node):
                 self.create_publisher(MarkerArray, "/particles/markers", 10)
         
 
-        self.odomSub = message_filters.Subscriber(self, Odometry, "/odom", qos_profile=qos_profile_laserScanner)
-        self.laserScanSub = message_filters.Subscriber(self, LaserScan, "/scan", qos_profile=qos_profile_laserScanner)
+        self.odomSub = message_filters.Subscriber(self, Odometry, "/odom",
+                                                   qos_profile=qos_profile_laserScanner)
+        self.laserScanSub = message_filters.Subscriber(self, LaserScan, "/scan",
+                                                        qos_profile=qos_profile_laserScanner)
 
+
+        self.initialPoseSubsriber = self.create_subscription(PoseWithCovarianceStamped, "/initialpose", self.initialPose2Dcallback ,10)
         self.timeSynchronizer = message_filters.ApproximateTimeSynchronizer([self.odomSub, self.laserScanSub], queue_size=10, slop=0.1)
         self.timeSynchronizer.registerCallback(self.filterCallback)
 
         self.publisher = self.create_publisher(OccupancyGrid, '/likelihood_map', 10)
         
-        self.mapUtilities=mapManipulator(mapFilename, laser_sig=0.2)
+        self.mapUtilities=mapManipulator(mapFilename, laser_sig=0.1)
         self.mapUtilities.make_likelihood_field()
 
 
         self.historyOdom = []
         self.particles=[]
 
-
-        width = -3
-        height = -1
+        self.numParticles = numParticles
+        self.std_particle_x = 0.5
+        self.std_particle_y = 0.5
 
         self.br = TransformBroadcaster(self)
 
-        self.particlePoses=np.random.uniform(low=[-3, -1.0, - 0.3], high=[-1.5, 0.0, + 0.3], size=(numParticles, 3))
+        print("give me your first estimate on rviz, 2D Pose Estimator")
+
+        self.initialized = False
+    
+    def initializeParticleFilter(self, x, y, th):
+        
+        numParticles = self.numParticles
+        self.particlePoses=np.random.uniform(low=[x-self.std_particle_x, y - self.std_particle_y, th - 0.3],
+                                             high=[x+self.std_particle_x, y + self.std_particle_y, th + 0.3], size=(numParticles, 3))
         
         self.particles = [particle(particle_, 1/numParticles) for particle_ in\
                            self.particlePoses]
         
 
         self.weights = [1/numParticles] * numParticles
+
+        self.initialized = True
+
+    def initialPose2Dcallback(self, marker: PoseWithCovarianceStamped):
+        
+        x, y, th = marker.pose.pose.position.x,\
+                   marker.pose.pose.position.y, euler_from_quaternion(marker.pose.pose.orientation)
+        
+
+        self.initializeParticleFilter(x,y,th)
+
     
     def visualizeParticles(self, particles_, stamp):
         
@@ -123,6 +146,10 @@ class particleFilter(Node):
         start_timer = time.time()
 
 
+        if not self.initialized:
+            return
+
+
         self.historyOdom.append(odomMsg)
 
         if len(self.historyOdom) > 2:
@@ -154,7 +181,7 @@ class particleFilter(Node):
 
         weighted_average_pose = np.append(weighted_average_translation, mean_angle)
         stamp = odomMsg.header.stamp
-        publishTransform(self.br, weighted_average_pose[0], weighted_average_pose[1], weighted_average_pose[2] -np.pi/2, stamp)
+        publishTransform(self.br, weighted_average_pose[0], weighted_average_pose[1], weighted_average_pose[2], stamp)
 
         self.publisher.publish(self.mapUtilities.to_message())
         self.normalizeWeights()
